b6f1076ae0d16ce634fe2eff42c7431527344fa3
ravig252525 <44760145+ravig252525@users.noreply.github.com>
2025-08-25 17:16:18 +0530

    first commit

---
+++ src/main/java/com/backend/screener/ScreenerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.screener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/screener/ScreenerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.screener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
